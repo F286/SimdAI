e25a544a46f6fd55a12886407e10880ca5124a0b
F286 <freedebreuil@gmail.com>
2023-12-25 17:35:49 -0600

    move simd tests to separate file

--- SimdAI/SimdAI.cpp
+++ SimdAI/SimdAI.cpp
@@ -3,33 +3,6 @@
 #include "core/simd.h"
 #include "core/tensor.h"
 
-TEST_CASE("SIMD operations", "[simd]") {
-    SECTION("Sum and Min Reduction") {
-        simd<float> x{ 1.0f, -2.0f, 3.0f, -4.0f, 5.0f, -6.0f, 7.0f, -8.0f };
-        float sum = reduce(x);
-        float min_val = reduce_min(x);
-
-        CHECK(sum == Approx(-4.0f));
-        CHECK(min_val == Approx(-8.0f));
-    }
-
-    SECTION("Conditional Selection") {
-        simd<float> x{ 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f };
-        simd<float> y{ 8.0f, 7.0f, 6.0f, 5.0f, 4.0f, 3.0f, 2.0f, 1.0f };
-
-        simd<float> output = simd_select(x < y, y, x);
-
-        for (size_t i = 0; i < x.size(); ++i) {
-            CHECK(output[i] == std::max(x[i], y[i]));
-        }
-    }
-
-    SECTION("Subscript Operator") {
-        simd<float> x{ 1.0f, 2.0f, 3.0f, 4.0f, 5.0f, 6.0f, 7.0f, 8.0f };
-        CHECK(x[2] == Approx(3.0f));
-    }
-}
-
 TEST_CASE("Tensor operations", "[tensor]") {
     Tensor<float> tensor({ 2, 3, 4 });
 
